ef26e1f70939058acac8551bb9b568503ebfcc30
Eunjin03 <ibsuny3214@naver.com>
2026-09-14 23:05:50 +0900

    Fix: 비로그인 상태일 경우 미션을 불러오지 않음 #11

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
   const [showLoginModal, setShowLoginModal] = useState(false);
 
   const { data: hotData } = useHotStores();
-  const { data: missionData } = useMissions();
+  const { data: missionData } = useMissions(isLoggedIn);
 
   const notLoginMissionData = {
     missions: [
