5f3f2e2f281d2336522cbd16f08bc76cbfcd66ee
Eunjin03 <ibsuny3214@naver.com>
2026-09-15 00:06:14 +0900

    Fix: 비로그인 상태일 경우 미션을 불러오지 않음 #11

--- src/hooks/api/useGetMissionAtHome.ts
+++ src/hooks/api/useGetMissionAtHome.ts
@@ -3,9 +3,10 @@ import { getMissions } from "../../api/home";
 
 export const missionsQueryKey = ["missions"] as const;
 
-export function useMissions() {
+export function useMissions(isLoggedIn: boolean) {
   return useQuery({
     queryKey: [...missionsQueryKey],
     queryFn: () => getMissions(),
+    enabled: isLoggedIn,
   });
 }
