685042ac41516114f7407cde71e24709344f3017
Eunjin03 <ibsuny3214@naver.com>
2026-09-17 00:31:12 +0900

    FEAT: 컬렉션 페이지 디자인 제작중 #17

--- src/pages/mainCollection/MainCollection.tsx
+++ src/pages/mainCollection/MainCollection.tsx
@@ -1,7 +1,84 @@
+// libraries
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// hooks
+import useIsLoggedIn from "../../hooks/useIsLoggedIn";
+// components
+import LoginModal from "../../components/modal/LoginModal";
+import PageHeader from "../../components/common/PageHeader";
+import Button from "../../components/Button";
+import SearchInput from "./components/SearchInput";
+// utils
+import { startKakaoLogin } from "../../utils/kakao";
+// assets
+import book from "../../assets/icon/book-sub-01.svg";
+import camera from "../../assets/icon/camera-white.svg";
+import plus from "../../assets/icon/plus.svg";
+// contexts
+import { useResponsive } from "../../contexts/ResponsiveContext";
+
 export default function MainCollectionPage() {
+  const isLoggedIn = useIsLoggedIn();
+  const navigate = useNavigate();
+
+  const { isMobile } = useResponsive();
+
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
   return (
-    <div>
-      <h1>대시보드</h1>
+    <div
+      className={`  flex flex-col items-start justify-center max-w-7xl mx-auto ${isMobile ? "p-4 gap-6" : "p-6 gap-8"} mb-40`}
+    >
+      {isMobile ? (
+        <div className="absolute bottom-5 right-5 shadow-md rounded-full bg-sub-01 flex items-center justify-center w-10 h-10">
+          <img src={plus} alt="플러스" className="w-6 h-6" />{" "}
+        </div>
+      ) : null}
+      <div className="flex items-center justify-between w-full">
+        <PageHeader
+          title="나의 빵 컬렉션"
+          icon={book}
+          subTitle="영수증을 인증하여 지금까지 다녀온 빵집 기록들을 모아보세요"
+          noBackButton={true}
+        />
+        {!isMobile && (
+          <Button
+            onClick={() => {
+              if (isLoggedIn) {
+                // navigate to collection page
+              }
+            }}
+            className="flex items-center gap-2 rounded-[10px] bg-primary py-2 px-4 text-white"
+          >
+            <img
+              src={camera}
+              alt="카메라"
+              className={` ${isMobile ? "typo-head-03" : "w-5 h-5"}`}
+            />
+            <p className={` ${isMobile ? "typo-head-03" : "text-base"}`}>
+              영수증으로 방문 인증
+            </p>
+          </Button>
+        )}
+      </div>
+      <SearchInput
+        onSearch={(keyword) => {
+          console.log("검색어:", keyword);
+        }}
+      />
+      {isLoggedIn ? (
+        <h1>대시보드</h1>
+      ) : (
+        <LoginModal
+          isMobile={isMobile}
+          onClick={startKakaoLogin}
+          description="로그인하고 나만의 빵집 기록을 모아 
+          빵 컬렉션을 만들어보세요"
+          onClose={() => {
+            navigate("/");
+          }}
+        />
+      )}
     </div>
   );
 }
