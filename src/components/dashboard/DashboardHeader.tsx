98fa498be27ed27e63d2fc905d029cab0c8433b8
ghida5130 <ghida5130@gmail.com>
2026-09-20 17:22:05 +0900

    STYLE: 기본 프로필 이미지, 서비스 로고 변경

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -1,5 +1,5 @@
 import cameraImage from "@/assets/images/dashboardPage/camera.svg";
-import logoImage from "@/assets/icon/logo.svg";
+import logoImage from "@/assets/icon/d.png";
 import { Link } from "react-router-dom";
 
 type DashboardHeaderProps = {
@@ -8,19 +8,30 @@ type DashboardHeaderProps = {
   profileImageUrl: string | null;
 };
 
-export default function DashboardHeader({ isMobile, nickname, profileImageUrl }: DashboardHeaderProps) {
+export default function DashboardHeader({
+  isMobile,
+  nickname,
+  profileImageUrl,
+}: DashboardHeaderProps) {
   return (
     <section className="flex">
       <div className="flex gap-3 items-center">
         <div className="size-10 rounded-full overflow-hidden">
-          <img className="size-full object-cover" src={profileImageUrl ?? logoImage} alt={`${nickname} 프로필`} />
+          <img
+            className="size-full object-cover"
+            src={profileImageUrl ?? logoImage}
+            alt={`${nickname} 프로필`}
+          />
         </div>
         <h2 className="text-2xl font-extrabold text-bl ack-01">
           안녕, <span className="text-sub-01">{nickname}</span>!
         </h2>
       </div>
       {!isMobile && (
-        <Link to="/receipt/verify" className="flex gap-2 ml-auto px-8 py-4 bg-sub-01 text-white rounded-2xl typo-head-03">
+        <Link
+          to="/receipt/verify"
+          className="flex gap-2 ml-auto px-8 py-4 bg-sub-01 text-white rounded-2xl typo-head-03"
+        >
           <img src={cameraImage} />
           <p>영수증으로 방문 인증</p>
         </Link>
